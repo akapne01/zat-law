e0bdfcc87e5661a0e1e546f0cd69faeaf120b4a6
Agi <akapne01@gmail.com>
2018-12-09 19:56:01 +0000

    Create vars to be displayed in HTML

--- web_build.py
+++ web_build.py
@@ -1,32 +1,41 @@
 # RUN THIS FILE IN TERMINAL USING python and file name to open the local host,
 # then open the the web address given in your browser
-import json, requests, urllib2
+import json, requests, urllib2, os
 from flask import Flask, render_template
 from random import randint
 
 app = Flask(__name__)
 
+# api_key = os.environ["API_KEY"]
+
 # Links API and reads count from api
 url = 'https://api.case.law/v1/cases/'
+# opens case url
 law_case = urllib2.urlopen(url)
+# reads case url
 wjson = law_case.read()
 wjdata = json.loads(wjson)
 
-# creates variable count that can be passed further to html as a var
-# generates random number in interval 0 to 99 and passes it to select the
-# relevant case
-random_case_number = randint(0, 99)
+# Funtions returning random number between 0 and 99
+def random_number():
+    return randint(0, 99)
+
+# assigns random number to the case using random_number() function
+random_case_number = random_number()
+# gets assigns id number of random case to the variable
 api_id = wjdata['results'][random_case_number]['id']
-# api_url = wjdata['results'][random_case_number]['url']'['/?full_case=true&format=html']
+# gets and assigns name of the case to variable
 api_name = wjdata['results'][random_case_number]['name']
+# gets and assigns url of this case to var
 api_url = 'https://api.case.law/v1/cases/{}/?full_case=true&format=html'.format(api_id)
+# gets and assigns decidion date of this case to var
 api_decision_date = wjdata['results'][random_case_number]['decision_date']
 
 # Returns index.html when goes to the main page or homepage
 @app.route("/")
 @app.route("/home")
 def home():
-    #passes count as variable in index.html
+    # passes variables got on this file to be used in index.html
     return render_template('index.html', title="Home Page", id = api_id, url = api_url, name = api_name,
     decision_date= api_decision_date)
 
